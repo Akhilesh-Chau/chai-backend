2aa9e2fcfbfa0d2192910eb4c1f7da507d9e815e
Akhilesh-Chau <akhileshch420@gmail.com>
2026-01-27 17:25:51 +0530

    all setup configured

--- src/util/asyncHandler.js
+++ src/util/asyncHandler.js
@@ -1,6 +1,6 @@
 // promise login
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
